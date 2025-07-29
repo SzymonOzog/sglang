5703f8c2f9449336689ed5e3f33bf756ca998c5c
SzymonOzog <szymon.ozog@gmail.com>
2025-07-29 04:11:11 -0700

    test

--- python/sglang/srt/layers/quantization/fp8_utils.py
+++ python/sglang/srt/layers/quantization/fp8_utils.py
@@ -252,6 +252,7 @@ def deepgemm_w8a8_block_fp8_linear_with_fallback(
         close = torch.allclose(x_scale, x_scale2)
         idx2 = torch.isclose(x_scale, x_scale2)
         logger.info(f"x = {inx}, q_input ={q_input[idx.logical_not()]}, 2 = {q_input2[idx.logical_not()]} allclose inputs {close}, scales {torch.allclose(x_scale, x_scale2)} x_scale ={x_scale[idx2.logical_not()]}, 2 = {x_scale2[idx2.logical_not()]}")
+        q_input, x_scale = q_input2, x_scale2
         if not close:
             torch.save(inx, "failed.pt")
     else:
