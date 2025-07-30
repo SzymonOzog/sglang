3c7f2cd3fd3cfbccb156d33a7a452f9e5ddaf98e
SzymonOzog <szymon.ozog@gmail.com>
2025-07-30 00:44:33 -0700

    test

--- python/sglang/srt/layers/quantization/fp8_utils.py
+++ python/sglang/srt/layers/quantization/fp8_utils.py
@@ -249,11 +249,16 @@ def deepgemm_w8a8_block_fp8_linear_with_fallback(
             scale_tma_aligned=True,
             scale_ue8m0=deep_gemm_wrapper.DEEPGEMM_SCALE_UE8M0,
         )
+        out2 = (q_input.to(torch.bfloat16)*dq).to(torch.bfloat16)
+
+        dq = x_scale2.repeat_interleave(128).reshape((q_input.shape))
+        out3 = (q_input2.to(torch.bfloat16)*dq).to(torch.bfloat16)
+
         close = torch.allclose(q_input.to(torch.bfloat16), q_input2.to(torch.bfloat16), atol=1, rtol = 1e-3)
         idx = torch.isclose(q_input.to(torch.bfloat16), q_input2.to(torch.bfloat16), atol=1, rtol = 1e-3)
-        close = torch.allclose(out2, out, atol=1e-1, rtol=1e-3)
+        close = torch.allclose(out2, out3, atol=1e-1, rtol=1e-3)
         idx2 = torch.isclose(x_scale, x_scale2)
-        logger.info(f"out = {out}, dequant={out2}, q_input ={q_input[idx.logical_not()]}, 2 = {q_input2[idx.logical_not()]} allclose inputs {close}, scales {torch.allclose(x_scale, x_scale2)} x_scale ={x_scale[idx2.logical_not()]}, 2 = {x_scale2[idx2.logical_not()]}")
+        logger.info(f"out = {out}, dequant={out2}, dequant2={out3}, q_input ={q_input[idx.logical_not()]}, 2 = {q_input2[idx.logical_not()]} allclose inputs {close}, scales {torch.allclose(x_scale, x_scale2)} x_scale ={x_scale[idx2.logical_not()]}, 2 = {x_scale2[idx2.logical_not()]}")
         q_input, x_scale = q_input2, x_scale2
         if not close:
             torch.save(inx, "failed.pt")
