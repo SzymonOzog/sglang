4e8c6d280b25956e5d3fd2ffaec43d42e9f0329c
SzymonOzog <szymon.ozog@gmail.com>
2025-09-03 14:33:02 +0000

    More profiling stuff

--- test_moe.py
+++ test_moe.py
@@ -1,4 +1,5 @@
 import torch
+from torch.profiler import profile, ProfilerActivity, record_function
 from sglang.srt.layers.moe.fused_moe_triton.fused_moe import fused_experts
 
 def get_stats(activated_experts):
@@ -7,12 +8,21 @@ def get_stats(activated_experts):
 
     mem_1 = activated_experts*w1.shape[1]*w1.shape[2] * w1.element_size() + \
             activated_experts*w1.shape[1]//block_shape[0]*w1.shape[2]//block_shape[0] * w1_scale.element_size() + \
-            x.numel() + num_tokens*x.shape[1]//block_shape[0] * 4
+            x.numel() + num_tokens*x.shape[1]//block_shape[0] * 4 + \
+            top_k * num_tokens * w1.shape[2] * 2
 
     mem_2 = activated_experts*w2.shape[1]*w2.shape[2] * w2.element_size() + \
             activated_experts*w2.shape[1]//block_shape[0]*w2.shape[2]//block_shape[0] * w2_scale.element_size() + \
-            x.numel() + activated_experts*num_tokens*x.shape[1]//block_shape[0] * 4
+            x.numel() + activated_experts*num_tokens*x.shape[1]//block_shape[0] * 4 + \
+            num_tokens * w2.shape[2] * 2
     return flops_1, flops_2, mem_1, mem_2
+
+def get_times(kernel_name, prof):
+    ret = []
+    for e in prof.profiler.function_events:
+        if kernel_name == e.name:
+            ret.append(e.device_time_total)
+    return ret
 
 torch.manual_seed(42)
 (w1, w2, w1_scale, w2_scale, moe_config) = torch.load("./python/sglang/moe_config.pt", weights_only=False)
@@ -29,27 +39,48 @@ top_k = 9 # 8 picked + 1 shared
 block_shape = [128, 128]
 n_experts = 257
 
-x = torch.randn((num_tokens, hidden_size), dtype=torch.bfloat16)
-topk_weights = torch.randn((num_tokens, top_k), dtype=torch.bfloat16)
+for num_tokens in [8, 256, 1024, 8192]:
+    print("Batch size", num_tokens)
+    x = torch.randn((num_tokens, hidden_size), dtype=torch.bfloat16)
+    topk_weights = torch.randn((num_tokens, top_k), dtype=torch.bfloat16)
 
 # Ideal
-topk_ids = torch.arange(top_k).repeat(num_tokens,1).to(torch.int32)
+    print("benchmarking ideal")
+    topk_ids = torch.arange(top_k).repeat(num_tokens,1).to(torch.int32)
 
-out = fused_experts(x, w1, w2, (topk_weights, topk_ids, None), moe_config,
-              use_fp8_w8a8=True, w1_scale=w1_scale, w2_scale=w2_scale, block_shape=block_shape)
-print(get_stats(top_k))
+    with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
+        out = fused_experts(x, w1, w2, (topk_weights, topk_ids, None), moe_config,
+                      use_fp8_w8a8=True, w1_scale=w1_scale, w2_scale=w2_scale, block_shape=block_shape)
+    f1,f2, m1,m2 = get_stats(top_k)
+    times = get_times("fused_moe_kernel", prof)
+    print(f"moe up {(f1/1e6)/(times[0]):.2f} TFLOPs, {(m1/1e3)/times[0]} GB/s")
+    print(f"moe down {(f2/1e6)/(times[1]):.2f} TFLOPs, {(m2/1e3)/times[1]} GB/s")
+    print(f1, f2, m1, m2)
 
 # Uniform
-topk_ids = (torch.arange(top_k*num_tokens)%n_experts).reshape(num_tokens, top_k).to(torch.int32)
+    print("benchmarking uniform")
+    topk_ids = (torch.arange(top_k*num_tokens)%n_experts).reshape(num_tokens, top_k).to(torch.int32)
 
-out = fused_experts(x, w1, w2, (topk_weights, topk_ids, None), moe_config,
-              use_fp8_w8a8=True, w1_scale=w1_scale, w2_scale=w2_scale, block_shape=block_shape)
+    with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
+        out = fused_experts(x, w1, w2, (topk_weights, topk_ids, None), moe_config,
+                      use_fp8_w8a8=True, w1_scale=w1_scale, w2_scale=w2_scale, block_shape=block_shape)
 
-print(get_stats(max(top_k*num_tokens, n_experts)))
+    f1,f2, m1,m2 = get_stats(min(top_k*num_tokens, n_experts))
+    times = get_times("fused_moe_kernel", prof)
+    print(f"moe up {(f1/1e6)/(times[0]):.2f} TFLOPs, {(m1/1e3)/times[0]} GB/s")
+    print(f"moe down {(f2/1e6)/(times[1]):.2f} TFLOPs, {(m2/1e3)/times[1]} GB/s")
+
 # Random
-topk_ids = torch.randint(low=0, size=(num_tokens, top_k), high=n_experts).to(torch.int32)
+    print("benchmarking random")
+    topk_ids = torch.randint(low=0, size=(num_tokens, top_k), high=n_experts).to(torch.int32)
 
-out = fused_experts(x, w1, w2, (topk_weights, topk_ids, None), moe_config,
-              use_fp8_w8a8=True, w1_scale=w1_scale, w2_scale=w2_scale, block_shape=block_shape)
+    with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
+        out = fused_experts(x, w1, w2, (topk_weights, topk_ids, None), moe_config,
+                      use_fp8_w8a8=True, w1_scale=w1_scale, w2_scale=w2_scale, block_shape=block_shape)
 
-print(get_stats(len(set(topk_ids.flatten().tolist()))))
+    f1,f2, m1,m2 = get_stats(len(set(topk_ids.flatten().tolist())))
+    times = get_times("fused_moe_kernel", prof)
+    print(f"moe up {(f1/1e6)/(times[0]):.2f} TFLOPs, {(m1/1e3)/times[0]} GB/s")
+    print(f"moe down {(f2/1e6)/(times[1]):.2f} TFLOPs, {(m2/1e3)/times[1]} GB/s")
+    print()
+    print()
